d68e4ce36822e5e62ae944f35bd005ed0477db5f
eliran menachem <eliran.menachem@gmail.com>
2020-01-29 19:27:10 +0200

    Fetch API

--- app.js
+++ app.js
@@ -2,23 +2,57 @@ document.getElementById("buttonText").addEventListener("click", getText);
 document.getElementById("buttonJson").addEventListener("click", getJson);
 document.getElementById("buttonAPI").addEventListener("click", getApi);
 
+// Get local text data
 function getText(e) {
   fetch("test.txt")
     .then(function(res) {
       return res.text();
     })
     .then(function(data) {
-      console.log(data);
+      document.getElementById("output").innerHTML = data;
     })
     .catch(function(err) {
-      console.log(err);
+      document.getElementById("output").innerHTML = err;
     });
 
   e.preventDefault();
 }
+
+// Get local json data
 function getJson(e) {
+  fetch("posts.json")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      let output = "";
+      data.forEach(function(data) {
+          output += `<div>${data.title} - ${data.body}</div>`
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      document.getElementById("output").innerHTML = err;
+    });
+
   e.preventDefault();
 }
+
+// Get date from external API
 function getApi(e) {
+    fetch("https://api.github.com/users")
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(data) {
+      let output = "";
+      data.forEach(function(user) {
+          output += `<div>${user.login}</div>`
+      });
+      document.getElementById("output").innerHTML = output;
+    })
+    .catch(function(err) {
+      document.getElementById("output").innerHTML = err;
+    });
   e.preventDefault();
 }
